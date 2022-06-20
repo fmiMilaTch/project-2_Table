f98076b6f50a9d0c995bbc12195b71a8d61cd47c
fmiMilaTch <104836126+fmiMilaTch@users.noreply.github.com>
2022-06-20 23:54:48 +0300

    Update FormulaCell.hpp

--- FormulaCell.hpp
+++ FormulaCell.hpp
@@ -1,23 +1,15 @@
 #include"cell.hpp"
 class FormulaCell : public Cell{
     double numValue;
-    unsigned** references;
-    unsigned refrencesNumber;
-    unsigned refCapacity=10;
-
-    void copyFrom(const FormulaCell&);
-    void del();
 
     friend int convertToFullNum(const char* text, int from, int to);
     friend bool isFormula(const char*);
     friend double convertEquasion(const char*);
 public:
-    FormulaCell(const char*);///
-    FormulaCell(const FormulaCell&);///
-    FormulaCell& operator=(const FormulaCell&);///
+    FormulaCell(const char*);
+    FormulaCell& operator=(const FormulaCell&);
     Cell* clone() const;
     double getValue() const;
     void print() const;
-    ~FormulaCell();///
 
 };
